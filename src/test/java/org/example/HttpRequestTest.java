adf4ceac14e27fcfa1cf30989932999c72ae4e81
saksham-s1ngh <saksham249singh@gmail.com>
2024-04-03 19:55:04 -0400

    Removed fluff from app behaviour test

--- src/test/java/org/example/HttpRequestTest.java
+++ src/test/java/org/example/HttpRequestTest.java
@@ -24,9 +24,8 @@ public class HttpRequestTest {
 
 //      Object returnedObject = restTemplate.getForObject("http://localhost:" + port + "/double?number=" + input, Integer.class);
 //      System.out.println(returnedObject);
-        //assertThat(restTemplate.getForObject("http://localhost:" + port + "/double?input=" + input, String.class).contains("54"));
 
-        //assertThat(result).isEqualTo(54);
+//      assertThat(restTemplate.getForObject("http://localhost:" + port + "/double?input=" + input, String.class).contains("54"));
 
         assertThat(restTemplate.getForObject("http://localhost:" + port + "/double?number=" + input, Integer.class).equals(54));
     }
